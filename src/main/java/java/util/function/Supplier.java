e5034193bb778cae469b5d16bb22ddd556b43336
yaocs2 <yaocs2@midea.com>
2020-12-02 16:11:29 +0800

    my-jdk-8-source> Supplier.java注释

--- src/main/java/java/util/function/Supplier.java
+++ src/main/java/java/util/function/Supplier.java
@@ -25,18 +25,28 @@
 package java.util.function;
 
 /**
+ * 20201202
+ * A. 表示结果的提供者。
+ * B. 每一次都有不同的supplier被调用或没有新的结果。
+ * C. 这是一个<a href=“package-summary.html“>函数接口</a>，其函数方法是{@link #get（）}。
+ */
+/**
+ * A.
  * Represents a supplier of results.
  *
+ * B.
  * <p>There is no requirement that a new or distinct result be returned each
  * time the supplier is invoked.
  *
+ * C.
  * <p>This is a <a href="package-summary.html">functional interface</a>
  * whose functional method is {@link #get()}.
  *
- * @param <T> the type of results supplied by this supplier
+ * @param <T> the type of results supplied by this supplier // 20201202 supplier提供的结果类型
  *
  * @since 1.8
  */
+// 20201202 表示结果的提供者
 @FunctionalInterface
 public interface Supplier<T> {
 
@@ -45,5 +55,6 @@ public interface Supplier<T> {
      *
      * @return a result
      */
+    // 20201202 得到一个结果。
     T get();
 }
