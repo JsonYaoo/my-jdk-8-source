b6a8175bfe8ddc767e64aeef6368747115fe12a6
yaocs2 <yaocs2@midea.com>
2020-12-07 13:47:23 +0800

    my-jdk-8-source> WildcardType.java注释

--- src/main/java/com/sun/javadoc/WildcardType.java
+++ src/main/java/com/sun/javadoc/WildcardType.java
@@ -1,44 +1,35 @@
 /*
  * Copyright (c) 2003, Oracle and/or its affiliates. All rights reserved.
  * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
  */
 
 package com.sun.javadoc;
 
-
 /**
- * Represents a wildcard type argument.
- * Examples include:    <pre>
+ * 20201207
+ * A. 表示通配符类型参数。 示例包括：
+ *      {@code <?>}
+ *      {@code <? extends E>}
+ *      {@code <? super T>}
+ * B. 通配符类型可以具有明确的extends界限或明确的super界限，也可以没有，但不能同时具有。
+ */
+/**
+ * A.
+ * Represents a wildcard type argument. Examples include:
+ * <pre>
  * {@code <?>}
  * {@code <? extends E>}
  * {@code <? super T>}
  * </pre>
+ *
+ * B.
  * A wildcard type can have explicit <i>extends</i> bounds
  * or explicit <i>super</i> bounds or neither, but not both.
  *
  * @author Scott Seligman
  * @since 1.5
  */
+// 20201207 表示通配符类型参数: {@code <?>} | {@code <? extends E>} | {@code <? super T>}
 public interface WildcardType extends Type {
 
     /**
