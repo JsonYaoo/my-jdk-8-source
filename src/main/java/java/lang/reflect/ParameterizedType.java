27d15ca908a6540351f4b6ed977221f70cae5d11
yaocs2 <yaocs2@midea.com>
2020-12-07 13:44:22 +0800

    my-jdk-8-source> ParameterizedType#getActualTypeArguments()注释

--- src/main/java/java/lang/reflect/ParameterizedType.java
+++ src/main/java/java/lang/reflect/ParameterizedType.java
@@ -1,35 +1,23 @@
 /*
  * Copyright (c) 2003, 2004, Oracle and/or its affiliates. All rights reserved.
  * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
  */
 
 package java.lang.reflect;
 
-
 /**
+ * 20201207
+ * A. ParameterizedType表示参数化类型，例如Collection <String>
+ * B. 如该程序包中所指定的，在反射方法第一次使用它时，将创建一个参数化类型。 创建参数化类型p时，将解析p实例化的通用类型声明，并以递归方式创建p的所有类型参数。
+ *    有关类型变量的创建过程的详细信息，请参见{@link java.lang.reflect.TypeVariable TypeVariable}。 重复创建参数化类型无效。
+ * C. 实现此接口的类的实例必须实现equals（）方法，该方法使共享相同泛型类型声明并具有相同类型参数的任何两个实例相等。
+ */
+/**
+ * A.
  * ParameterizedType represents a parameterized type such as
  * Collection&lt;String&gt;.
  *
+ * B.
  * <p>A parameterized type is created the first time it is needed by a
  * reflective method, as specified in this package. When a
  * parameterized type p is created, the generic type declaration that
@@ -38,23 +26,33 @@ package java.lang.reflect;
  * TypeVariable} for details on the creation process for type
  * variables. Repeated creation of a parameterized type has no effect.
  *
+ * C.
  * <p>Instances of classes that implement this interface must implement
  * an equals() method that equates any two instances that share the
  * same generic type declaration and have equal type parameters.
  *
  * @since 1.5
  */
+// 20201207 ParameterizedType表示参数化类型 -> 在反射方法第一次使用它时，将创建一个参数化类型
 public interface ParameterizedType extends Type {
+
     /**
+     * 20201207
+     * A. 返回代表该类型的实际类型参数的{@code Type}对象的数组。
+     * B. 请注意，在某些情况下，返回的数组为空。 如果此类型表示嵌套在参数化类型内的非参数化类型，则会发生这种情况。
+     */
+    /**
+     * A.
      * Returns an array of {@code Type} objects representing the actual type
      * arguments to this type.
      *
+     * B.
      * <p>Note that in some cases, the returned array be empty. This can occur
      * if this type represents a non-parameterized type nested within
      * a parameterized type.
      *
-     * @return an array of {@code Type} objects representing the actual type
-     *     arguments to this type
+     * // 20201207 {@code Type}对象的数组，表示此类型的实际类型参数
+     * @return an array of {@code Type} objects representing the actual type arguments to this type
      * @throws TypeNotPresentException if any of the
      *     actual type arguments refers to a non-existent type declaration
      * @throws MalformedParameterizedTypeException if any of the
@@ -62,6 +60,7 @@ public interface ParameterizedType extends Type {
      *     be instantiated for any reason
      * @since 1.5
      */
+    // 20201207 返回代表该类型的实际类型参数的{@code Type}对象的数组。
     Type[] getActualTypeArguments();
 
     /**
