838d245cb6c4533b0309f62194e3079c80143f10
yaocs2 <yaocs2@midea.com>
2020-12-23 10:42:40 +0800

    my-jdk-8-source> Flushable.java注释

--- src/main/java/java/io/Flushable.java
+++ src/main/java/java/io/Flushable.java
@@ -1,26 +1,6 @@
 /*
  * Copyright (c) 2004, 2013, Oracle and/or its affiliates. All rights reserved.
  * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
  */
 
 package java.io;
@@ -28,12 +8,17 @@ package java.io;
 import java.io.IOException;
 
 /**
+ * 20201223
+ * 可刷新是可刷新数据的目标。 调用flush方法将任何缓冲的输出写入基础流。
+ */
+/**
  * A <tt>Flushable</tt> is a destination of data that can be flushed.  The
  * flush method is invoked to write any buffered output to the underlying
  * stream.
  *
  * @since 1.5
  */
+// 20201223 可刷新是可刷新数据的目标。 调用flush方法将任何缓冲的输出写入基础流。
 public interface Flushable {
 
     /**
